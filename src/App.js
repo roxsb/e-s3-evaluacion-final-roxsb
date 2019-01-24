aeabac81c04305ef303c97c5e1facab70c067267
roxsb <rox.san.bri@gmail.com>
2019-01-24 09:22:56 +0100

    create fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const ENDPOINT = 'http://hp-api.herokuapp.com/api/characters';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results : []
+    };
+
+    this.getCharacter();
+  }
+
+  getCharacter(){
+    fetch(ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({
+        results : data       
+      });
+    });
+  }
+  
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header">
+          <h1 className="title">Lista de personajes de la Saga Harry Potter</h1>
         </header>
+        <main className="main">
+          <div className="main__container">
+            <input className="main__input" type="text"></input>
+            <ul className="main__list">
+              <li className="list__item">
+                <img src="item__picture" alt="" />
+                <h2 className="item__name"> </h2>
+                <p className="item__house"></p>
+                <div className="item__moreinfo">
+                </div>
+              </li>
+            </ul>
+          </div>
+        </main>
       </div>
     );
   }
