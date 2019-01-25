03cfdab994deb9eece5ab4985f845dacad24e180
roxsb <rox.san.bri@gmail.com>
2019-01-25 13:34:45 +0100

    create docs

---
+++ docs/precache-manifest.6708f87d69e5b82e6d6be15e536fbb6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d7ae601d0cac56e34e06",
+    "url": "./static/js/main.d7ae601d.chunk.js"
+  },
+  {
+    "revision": "75dceaf7d4aa0ee98f02",
+    "url": "./static/js/1.75dceaf7.chunk.js"
+  },
+  {
+    "revision": "d7ae601d0cac56e34e06",
+    "url": "./static/css/main.880316cf.chunk.css"
+  },
+  {
+    "revision": "c94c6d0de4886baa92e7046a2f3f87b2",
+    "url": "./index.html"
+  }
+];
